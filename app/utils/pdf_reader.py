a0cd3932aff778c0d1a130739e84d73bf69d8246
Sumukha_Upadhyaya <sumukhaupadhyaya@gmail.com>
2025-03-03 10:06:52 +0530

    rebuilt webiste and added ai paper comparison feature

--- app/utils/pdf_reader.py
+++ app/utils/pdf_reader.py
@@ -1,9 +1,29 @@
-import PyPDF2
+from PyPDF2 import PdfReader
 
-def extract_text_from_pdf(pdf_path):
-    text=""
-    with open(pdf_path,"rb") as file:
-        reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            text+=page.extract_text()
-            return text
+def readPDF():
+    reference = ""
+    user = ""
+    template = ""
+    reader_r = PdfReader('Reference.pdf')  # replace with desired file
+    reader_u = PdfReader('User.pdf')
+    reader_t = PdfReader('Template.pdf')
+
+    n_pages_r = len(reader_r.pages)
+    n_pages_u = len(reader_u.pages)
+    n_pages_t = len(reader_t.pages)
+
+    for i in range(n_pages_r):
+        page_r = reader_r.pages[i]
+        reference += page_r.extract_text()
+
+    for i in range(n_pages_u):
+        page_u = reader_u.pages[i]
+        user += page_u.extract_text()
+
+        for i in range(n_pages_t):
+            page_t = reader_t.pages[i]
+            template += page_t.extract_text()
+
+
+    return reference,user,template
+
