36cde08550ff64195acf7f5051edca1a64af3b3e
snehal <snehalreddy@gmail.com>
2025-03-24 17:06:13 +0530

    final edit

--- app/static/js/compareFileHandler.js
+++ app/static/js/compareFileHandler.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const fileInput = document.querySelector('input[')
+document.addEventListener('DOMContentLoaded', function() {
+    const userFileInput = document.getElementById('user-research-file')
+    const referenceFileInput = document.getElementById('reference-research-file')
+    
+    function updateFileName(input) {
+        const fileName = input.files[0] ? input.files[0].name : "No file selected"
+        const fileNameDisplay = input.parentElement.querySelector('.file-name')
+        fileNameDisplay.textContent = fileName;
+    }
+    userFileInput.addEventListener('change', function() {
+        updateFileName(this);
+    })
+    referenceFileInput.addEventListener('change', function() {
+        updateFileName(this);
+    })
 })
